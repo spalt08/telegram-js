74446af79448f003034ee829a273c6453d74ce14
Sergey Mostsevenko <finesserus@gmail.com>
2019-11-17 14:29:33 +1000

    Reverse the messages order

--- src/components/routes/home/messages.ts
+++ src/components/routes/home/messages.ts
@@ -2,19 +2,34 @@ import { div } from 'core/html';
 import { message as service } from 'services';
 import message from 'components/message/message';
 import { list } from 'components/ui';
+import { BehaviorSubject } from 'rxjs';
+import { mount } from '../../../core/dom';
+import { useObservable } from '../../../core/hooks';
+
+function reverseIdsList<T>(ids: Readonly<T[]>): T[] {
+  const { length } = ids;
+  const reversed = new Array(length);
+  for (let i = 0; i < length; i += 1) {
+    reversed[length - i - 1] = ids[i];
+  }
+  return reversed;
+}
 
 export default function messages() {
   // todo: Add loading placeholder
 
-  return (
-    div`.messages`(
-      list({
-        className: 'messages__history',
-        items: service.history,
-        threshold: 1000,
-        batch: 100,
-        renderer: (id: number) => message(id, service.activePeer.value!),
-      }),
-    )
-  );
+  const element = div`.messages`();
+  const itemsSubject = new BehaviorSubject<number[]>([]);
+
+  useObservable(element, service.history, (history) => itemsSubject.next(reverseIdsList(history)));
+
+  mount(element, list({
+    className: 'messages__history',
+    items: itemsSubject,
+    threshold: 1000,
+    batch: 100,
+    renderer: (id: number) => message(id, service.activePeer.value!),
+  }));
+
+  return element;
 }
