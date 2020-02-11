bcf599d8ceeaa26619fb30d293ae4d2d82b11cf3
Mikhail Isupov <lam0x86@gmail.com>
2020-02-12 00:45:55 +0300

    Hide avatars in channels

--- src/components/message/message.ts
+++ src/components/message/message.ts
@@ -48,7 +48,7 @@ export default function message(uniqueId: string, peer: Peer) {
     let shouldRerender = false;
 
     // mount picture if it is first call
-    if ((!picture || !prev) && !out && peer._ !== 'peerUser') {
+    if ((!picture || !prev) && !out && peer._ !== 'peerUser' && msg.from_id !== 0) {
       const fromPeer = userIdToPeer(msg.from_id);
       picture = profileAvatar(fromPeer, next);
       title = div`.message__title${`color-${idToColorCode(next.from_id)}`}`(profileTitle(fromPeer));
