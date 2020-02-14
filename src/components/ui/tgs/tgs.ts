93cffae2507c2f5fa29ae66e9b9140d64eb2c794
Sergey Mostsevenko <finesserus@gmail.com>
2020-02-14 21:25:35 +1000

    fix: Application overloads CPU after fast switching between dialogs with stickers

--- src/components/ui/tgs/tgs.ts
+++ src/components/ui/tgs/tgs.ts
@@ -11,8 +11,9 @@ interface Props {
 }
 
 export default function tgs({ src, className, autoplay = true, loop = false }: Props) {
-  let animation: AnimationItem & { currentFrame: number};
-  let delayedPlay = false;
+  let animation: AnimationItem & { currentFrame: number} | undefined;
+  let isMounted = false;
+  let shouldPlay = autoplay;
 
   const container = div({ className });
 
@@ -22,27 +23,49 @@ export default function tgs({ src, className, autoplay = true, loop = false }: P
         container,
         loop,
         animationData,
-        autoplay: autoplay || delayedPlay,
+        autoplay: isMounted && shouldPlay,
         renderer: 'canvas',
       }) as AnimationItem & { currentFrame: number};
     });
   }
 
-  useOnUnmount(container, () => animation && animation.stop());
-  useOnMount(container, () => autoplay && animation && animation.play());
+  useOnMount(container, () => {
+    isMounted = true;
+    if (shouldPlay && animation) {
+      animation.play();
+    }
+  });
+  useOnUnmount(container, () => {
+    isMounted = false;
+    if (animation) {
+      animation.stop();
+    }
+  });
 
   return useInterface(container, {
-    pause() { if (animation) animation.pause(); delayedPlay = false; },
-    play() { if (animation) animation.play(); else delayedPlay = true; },
+    play() {
+      shouldPlay = true;
+      if (isMounted && animation) {
+        animation.play();
+      }
+    },
+    pause() {
+      shouldPlay = false;
+      if (animation) {
+        animation.pause();
+      }
+    },
     goTo(value: number, animate: boolean = false) {
-      if (animation.currentFrame === 0) {
-        animation.playSegments([0, value + 1], true);
-      } else if (value === 0) {
-        animation.playSegments([animation.currentFrame, 0], true);
-      } else if (animate) {
-        animation.playSegments([animation.currentFrame, value], true);
-      } else {
-        animation.goToAndStop(value, true);
+      if (animation) {
+        if (animation.currentFrame === 0) {
+          animation.playSegments([0, value + 1], true);
+        } else if (value === 0) {
+          animation.playSegments([animation.currentFrame, 0], true);
+        } else if (animate) {
+          animation.playSegments([animation.currentFrame, value], true);
+        } else {
+          animation.goToAndStop(value, true);
+        }
       }
     },
   });
