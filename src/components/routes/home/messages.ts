b89deeab68b1cf757d00d50134774b19b233f276
Sergey Mostsevenko <finesserus@gmail.com>
2020-02-15 02:48:25 +1000

    Attach the button to the onFocus event

--- src/components/routes/home/messages.ts
+++ src/components/routes/home/messages.ts
@@ -27,7 +27,7 @@ function prepareIdsList(peer: Peer, messageIds: Readonly<number[]>): string[] {
 }
 
 export default function messages({ className = '' }: Props = {}) {
-  const showDownButton = new BehaviorSubject(true); // todo: False initially
+  const showDownButton = new BehaviorSubject(false);
 
   const downButton = button({
     className: showDownButton.pipe(
@@ -62,7 +62,12 @@ export default function messages({ className = '' }: Props = {}) {
     renderer: (id: string) => message(id, service.activePeer.value!, (mid: string) => scroll.pendingRecalculate.push(mid)),
     onReachTop: () => service.loadMoreHistory(MessageDirection.Older),
     onReachBottom: () => service.loadMoreHistory(MessageDirection.Newer),
-    onFocus: (id: string) => console.log('inside focus', id),
+    onFocus: (id: string) => {
+      const showDown = !service.history.value.newestReached || itemsSubject.value[itemsSubject.value.length - 1] !== id;
+      if (showDown !== showDownButton.value) {
+        showDownButton.next(showDown);
+      }
+    },
   });
 
   mount(historySection, scroll.container);
@@ -72,7 +77,6 @@ export default function messages({ className = '' }: Props = {}) {
   // Handle message focus
   useObservable(element, service.focusMessage, (focus) => {
     if (service.activePeer.value) {
-      console.log('outside focus', focus);
       scroll.cfg.highlightFocused = focus.highlight || false;
       scroll.focus(
         peerMessageToId(service.activePeer.value, focus.id),
