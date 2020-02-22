31e2289fee77fe1210841c58cf4ace2435aa9866
Sergey Mostsevenko <finesserus@gmail.com>
2020-02-22 23:58:15 +1000

    Include to the bundle only the canvas Lottie engine
    https://github.com/spalt08/telegram-js/issues/42

--- src/lazy-modules/lottie.ts
+++ src/lazy-modules/lottie.ts
@@ -1,5 +1,5 @@
 // Don't import the Lottie types here otherwise the module wan't be lazy
 
 export default function loadLottie() {
-  return import(/* webpackChunkName: "lottie" */ 'lottie-web').then((module) => module.default);
+  return import(/* webpackChunkName: "lottie" */ 'lottie-web/build/player/lottie_canvas').then((module) => module.default);
 }
