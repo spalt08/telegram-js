c7a71061554724f30cbd90f18e31d93f1d73735a
Konstantin Darutkin <spalt08@mail.ru>
2020-03-03 00:28:19 +0300

    story for select

--- src/components/ui/select/select_autocomplete.story.ts
+++ src/components/ui/select/select_autocomplete.story.ts
@@ -4,6 +4,9 @@ import centered from '@storybook/addon-centered/html';
 import { array, text, withKnobs } from '@storybook/addon-knobs';
 import { action } from '@storybook/addon-actions';
 
+import country, { Country } from 'const/country';
+import { div, text as textNode } from 'core/html';
+import emoji from '../emoji/emoji';
 import selectAutoComplete from './select_autocomplete';
 
 const stories = storiesOf('II. UI Elements | Select', module)
@@ -16,5 +19,23 @@ stories.add('Common Usage', () => (
     selected: 0,
     options: array('Options', ['Orange', 'Apple', 'Pineapple']),
     onChange: action('input-changed'),
+  })
+));
+
+const countryOptionRenderer = ({ phone, label: countryLabel, emoji: emojiStr }: Country) => (
+  div`.logincountry`(
+    emoji(emojiStr, { className: 'logincountry__flag', lazy: true }),
+    div`.logincountry__label`(textNode(countryLabel)),
+    div`.logincountry__phone`(textNode(phone)),
+  )
+);
+
+stories.add('Custom Renderer', () => (
+  selectAutoComplete({
+    label: 'Country',
+    selected: 0,
+    options: country,
+    optionRenderer: countryOptionRenderer,
+    optionLabeler: (data) => data.label,
   })
 ));
