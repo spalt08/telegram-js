e4c7a87748ef5f2a04a2975d3e99848ab35ca537
Sergey Mostsevenko <finesserus@gmail.com>
2020-03-11 15:35:06 +1000

    Fix the avatar login

--- src/components/profile/avatar/avatar.ts
+++ src/components/profile/avatar/avatar.ts
@@ -2,7 +2,7 @@ import { div, text, img } from 'core/html';
 import { Peer, Message } from 'cache/types';
 import { peerToInitials, peerToColorCode } from 'cache/accessors';
 import { getPeerPhotoInputLocation } from 'helpers/photo';
-import { mount, unmountChildren } from 'core/dom';
+import { mount } from 'core/dom';
 import media from 'client/media';
 import { auth } from 'services';
 import { savedmessages, avatarDeletedaccount } from 'components/icons';
@@ -21,46 +21,28 @@ function isDeletedAccount(peer: Peer) {
   return false;
 }
 
-function pictureAvatar(src: string | null) {
-  const holder = div`.avatar__picture`();
-  if (src !== null) {
-    mount(holder, img({ src }));
-  }
-  return holder;
-}
-
-function picturelessAvatar(peer: Peer, isForDialogList: boolean) {
-  if (isMyself(peer) && isForDialogList) {
-    return div`.avatar__predefined`(
-      savedmessages(),
-    );
-  }
-  if (isDeletedAccount(peer)) {
-    return div`.avatar__predefined`(
-      avatarDeletedaccount(),
-    );
-  }
-  return div`.avatar__standard${`color-${peerToColorCode(peer)}`}`(
-    text(peerToInitials(peer)[0]),
-  );
-}
-
 export default function profileAvatar(peer: Peer, message?: Message, isForDialogList = false) {
   const container = div`.avatar`();
-  const location = getPeerPhotoInputLocation(peer, message);
 
-  if (location) {
-    media.get(location, (src) => {
-      unmountChildren(container);
-      mount(container, pictureAvatar(src));
-    });
-
-    // No cache – add placeholder
-    if (!container.firstChild) {
-      mount(container, pictureAvatar(null));
-    }
+  if (isForDialogList && isMyself(peer)) {
+    mount(container, div`.avatar__predefined`(
+      savedmessages(),
+    ));
+  } else if (isDeletedAccount(peer)) {
+    mount(container, div`.avatar__predefined`(
+      avatarDeletedaccount(),
+    ));
   } else {
-    mount(container, picturelessAvatar(peer, isForDialogList));
+    const location = getPeerPhotoInputLocation(peer, message);
+    if (location) {
+      const holder = div`.avatar__picture`();
+      mount(container, holder);
+      media.get(location, (src) => mount(holder, img({ src })));
+    } else {
+      mount(container, div`.avatar__standard${`color-${peerToColorCode(peer)}`}`(
+        text(peerToInitials(peer)[0]),
+      ));
+    }
   }
 
   return container;
