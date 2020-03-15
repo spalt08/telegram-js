441c0941335f819605a28b27432ca8581847af55
Sergey Mostsevenko <finesserus@gmail.com>
2020-03-15 12:45:18 +1000

    Amend the menu&back icon story

--- src/components/icons/menu_and_back/menu_and_back.story.ts
+++ src/components/icons/menu_and_back/menu_and_back.story.ts
@@ -4,16 +4,17 @@ import centered from '@storybook/addon-centered/html';
 import { withKnobs, color as colorKnob, number as numberKnob } from '@storybook/addon-knobs';
 import { withMountTrigger } from 'storybook/decorators';
 import { BehaviorSubject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { button, div, text } from 'core/html';
-import menuAndBack, { State } from './menu_and_back';
+import menuAndBack from './menu_and_back';
 
 const stories = storiesOf('UI Elements | Icons', module)
   .addDecorator(withKnobs)
   .addDecorator(withMountTrigger)
   .addDecorator(centered);
 
-const iconState = new BehaviorSubject<State>('menu');
+const iconState = new BehaviorSubject(false);
 
 stories.add('Menu\'n\'Back', () => {
   const color = colorKnob('Color', '#4EA4F5');
@@ -22,15 +23,12 @@ stories.add('Menu\'n\'Back', () => {
     max: 500,
   });
   const transitionDuration = numberKnob('Transition duration (ms)', -1);
-
-  const icon = menuAndBack({
-    state: iconState,
-    style: {
-      width: `${size}px`,
-      height: `${size}px`,
-      transitionDuration: transitionDuration >= 0 ? `${transitionDuration}ms` : '',
-    },
-  });
+  const iconStyle = {
+    width: `${size}px`,
+    height: `${size}px`,
+    transitionDuration: transitionDuration >= 0 ? `${transitionDuration}ms` : '',
+    margin: '0 5px 10px',
+  };
 
   return div(
     {
@@ -39,12 +37,21 @@ stories.add('Menu\'n\'Back', () => {
         textAlign: 'center',
       },
     },
-    div(icon),
+    menuAndBack({
+      state: iconState.pipe(map((value) => value ? 'back' : 'menu')),
+      style: iconStyle,
+    }),
+    menuAndBack({
+      state: iconState.pipe(map((value) => value ? 'menu' : 'back')),
+      style: iconStyle,
+    }),
     // todo: Move to a knob when Storybook is updated to version 6: https://github.com/storybookjs/storybook/issues/6705
-    button({
-      onClick() {
-        iconState.next(iconState.value === 'menu' ? 'back' : 'menu');
-      },
-    }, text('Toggle state')),
+    div(
+      button({
+        onClick() {
+          iconState.next(!iconState.value);
+        },
+      }, text('Toggle state')),
+    ),
   );
 });
