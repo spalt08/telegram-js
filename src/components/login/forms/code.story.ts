f114a3bcc36349a96e1e368339c50325063534ea
Konstantin Darutkin <spalt08@mail.ru>
2020-03-16 23:14:22 +0300

    feat(login): updated stories

--- src/components/login/forms/code.story.ts
+++ src/components/login/forms/code.story.ts
@@ -1,11 +1,19 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { storiesOf } from '@storybook/html';
-import centered from '@storybook/addon-centered/html';
-import { withMountTrigger } from 'storybook/decorators';
+import { withMountTrigger, centered } from 'storybook/decorators';
+import { auth, AuthStage } from 'services';
 import codeForm from './code';
 
 const stories = storiesOf('Login | Forms', module)
   .addDecorator(withMountTrigger)
   .addDecorator(centered);
 
-stories.add('Code', () => codeForm());
+stories.add('Code', () => {
+  auth.state.next(AuthStage.Code);
+  return codeForm();
+});
+
+stories.add('2FA', () => {
+  auth.state.next(AuthStage.TwoFA);
+  return codeForm();
+});
--- src/components/login/forms/profile.story.ts
+++ src/components/login/forms/profile.story.ts
@@ -1,7 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { storiesOf } from '@storybook/html';
-import centered from '@storybook/addon-centered/html';
-import { withMountTrigger } from 'storybook/decorators';
+import { withMountTrigger, centered } from 'storybook/decorators';
 import profileForm from './profile';
 
 const stories = storiesOf('Login | Forms', module)
