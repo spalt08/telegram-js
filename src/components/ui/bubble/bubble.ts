9437f9cee8077e8b4991f16b189e69a5eb51dc12
Mikhail Isupov <lam0x86@gmail.com>
2020-03-17 19:05:02 +0300

    Fixed bubbles for MacOS

--- src/components/ui/bubble/bubble.ts
+++ src/components/ui/bubble/bubble.ts
@@ -3,6 +3,11 @@ import { useInterface } from 'core/hooks';
 import './bubble.scss';
 import { unmount } from 'core/dom';
 
+// Chrome and Safari on MacOS scale up png masks, while Windows browsers do not.
+// At the same time, Windows browsers scale down svg masks, while MacOS browsers don't.
+// Thus, we use png masks for Windows and svg masks for MacOS.
+const isMac = navigator.platform.indexOf('Mac') >= 0;
+
 export type BubbleInterface = {
   updateBorders: (first: boolean, last: boolean) => void;
 };
@@ -11,7 +16,7 @@ export default function bubble(out: boolean, masked: boolean, onlyMedia: boolean
   const shadow = div`.bubble__shadow`();
   const background = div`.bubble__background`();
   const content = div`.bubble__content`(...children);
-  const element = div`.bubble${masked ? 'masked' : ''}`(shadow, background, content);
+  const element = div`.bubble${masked ? 'masked' : ''}${isMac ? 'svg' : ''}`(shadow, background, content);
 
   if (onlyMedia) {
     unmount(background);
