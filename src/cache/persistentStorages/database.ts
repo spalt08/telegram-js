0c0c919d1aff6d99a91e373714fe7ac4971b7da1
Sergey Mostsevenko <finesserus@gmail.com>
2020-06-01 02:11:47 +1000

    Screw migrations. Recreate the database on version change.
    It's more convenient during development, more reliable and doesn't require more and more code with new updates.

--- src/cache/persistentStorages/database.ts
+++ src/cache/persistentStorages/database.ts
@@ -1,27 +1,35 @@
 // This file only keeps the schema of the application indexedDB database and a connection to it
 
-// Add a migration to the array to change the schema. Don't forget that you must delete a store before creating it again
-// Don't edit or delete migrations. If you do, ask all your users to clear the indexedDB storage.
-const migrations: Array<(db: IDBDatabase) => void> = [
-  (db) => {
-    db.createObjectStore('meta');
-  },
-];
+const META_STORE_NAME = 'meta';
 
-function actualizeSchema(db: IDBDatabase, oldVersion: number, newVersion: number) {
-  for (let i = oldVersion; i < newVersion; ++i) {
-    if (migrations[i]) {
-      migrations[i](db);
+// Increment it when you change the schema below
+const SCHEMA_VERSION = 1;
+
+function makeSchema(db: IDBDatabase) {
+  // to be continued...
+}
+
+function actualizeSchema(db: IDBDatabase, oldVersion: number, _newVersion: number) {
+  if (oldVersion === 0) {
+    db.createObjectStore(META_STORE_NAME);
+  }
+
+  const stores = db.objectStoreNames;
+  for (let i = 0; i < stores.length; ++i) {
+    if (stores[i] !== META_STORE_NAME) { // not removed to not loose the session during development
+      db.deleteObjectStore(stores[i]);
     }
   }
+
+  makeSchema(db);
 }
 
 function openDatabase(): Promise<IDBDatabase> {
   return new Promise((resolve, reject) => {
-    const request = indexedDB.open('tgm', migrations.length);
+    const request = indexedDB.open('tgm', SCHEMA_VERSION);
 
     request.onupgradeneeded = (event: IDBVersionChangeEvent) => {
-      actualizeSchema(request.result, event.oldVersion, event.newVersion || migrations.length);
+      actualizeSchema(request.result, event.oldVersion, event.newVersion || SCHEMA_VERSION);
     };
 
     request.onsuccess = () => {
