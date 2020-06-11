9c8ae3dfa68ba790abb97a96e814e148829289ce
Sergey Mostsevenko <finesserus@gmail.com>
2020-06-11 23:29:36 +1000

    Extract the tab header component to a separate file

--- src/components/ui/tabs/tabs.ts
+++ src/components/ui/tabs/tabs.ts
@@ -1,10 +1,11 @@
 import { BehaviorSubject } from 'rxjs';
 import { distinctUntilChanged, map } from 'rxjs/operators';
-import { div, span, text } from 'core/html';
-import { listenOnce, unmount, mount, listen, animationFrameStart } from 'core/dom';
+import { div } from 'core/html';
+import { listenOnce, unmount, mount, animationFrameStart } from 'core/dom';
 import { useInterface, hasInterface, getInterface, useMaybeObservable } from 'core/hooks';
 import { MaybeObservable } from 'core/types';
 import simpleList from '../simple_list';
+import tabHeader from './tab_header';
 import './tabs.scss';
 
 interface TabsInterface {
@@ -31,55 +32,6 @@ type Props = {
   headerAlign?: 'center' | 'space-between' | 'stretch',
   hideHeader?: MaybeObservable<boolean>,
 };
-
-function tabHeader(
-  title: MaybeObservable<string>,
-  badge: MaybeObservable<string> = '',
-  isActive: MaybeObservable<boolean>,
-  onClick: () => void,
-) {
-  const titleNode = text('');
-  let badgeEl: HTMLElement | undefined;
-  const contentEl = div`.tabs-panel__tab_content`(titleNode);
-  const container = div`.tabs-panel__tab`(contentEl);
-
-  let releaseTitle: (() => void) | undefined;
-  let releaseBadge: (() => void) | undefined;
-
-  const setTitle = (newTitle: MaybeObservable<string>) => {
-    if (releaseTitle) releaseTitle();
-    releaseTitle = useMaybeObservable(container, newTitle, (titleString) => {
-      titleNode.textContent = titleString;
-    });
-  };
-
-  const setBadge = (newBadge: MaybeObservable<string>) => {
-    if (releaseBadge) releaseBadge();
-    releaseBadge = useMaybeObservable(container, newBadge, (badgeString) => {
-      if (badgeString) {
-        if (!badgeEl) {
-          badgeEl = span`.tabs-panel__tab_badge`();
-          mount(contentEl, badgeEl);
-        }
-        badgeEl.textContent = badgeString;
-      } else if (badgeEl) {
-        unmount(badgeEl);
-        badgeEl = undefined;
-      }
-    });
-  };
-
-  useMaybeObservable(container, isActive, (value) => {
-    container.classList.toggle('-active', value);
-  });
-
-  setTitle(title);
-  setBadge(badge);
-
-  listen(container, 'click', onClick);
-
-  return useInterface(container, { setTitle, setBadge });
-}
 
 export default function tabsPanel({ className = '', headerAlign = 'center', hideHeader }: Props, tabs: MaybeObservable<TabItem[]>) {
   let tabsMeta: Record<string, TabMeta> = {};
