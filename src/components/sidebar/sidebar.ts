9a9b59f3a80e79fa2b3fd45d65432a8d30c8635e
Sergey Mostsevenko <finesserus@gmail.com>
2020-06-18 21:57:41 +1000

    refactor: Sidebar context can be set through onNavigate, automatic type derive in the openModal method

--- src/components/sidebar/sidebar.ts
+++ src/components/sidebar/sidebar.ts
@@ -1,7 +1,9 @@
+import { BehaviorSubject } from 'rxjs';
 import { listen, mount, unmount } from 'core/dom';
 import { useInterface } from 'core/hooks';
 import { div } from 'core/html';
 import { main } from 'services';
+import { MaybeObservableValue } from 'core/types';
 import addBotToGroup from './add_bot_to_group/add_bot_to_group';
 import archive from './archived_dialogs/archived_dialogs';
 import contacts from './contacts_list/contacts_list';
@@ -34,15 +36,17 @@ const elements = {
 
 type SidebarRendererMap = typeof elements;
 export type SidebarState = keyof SidebarRendererMap;
+export type SidebarContext<TState extends SidebarState> = MaybeObservableValue<Parameters<SidebarRendererMap[TState]>[1]>;
+export type SidebarStateAndCtx = { [K in SidebarState]: { state: K, ctx: SidebarContext<K> } }[SidebarState];
 
 type Props = {
-  initial?: SidebarState,
+  initial?: SidebarStateAndCtx,
   className?: string,
   onTransitionStart?: (opening: boolean) => void,
 };
 
 export type SidebarComponentProps = {
-  onNavigate?: (state: SidebarState) => void;
+  onNavigate?: <K extends SidebarState>(state: K, ctx: SidebarContext<K>) => void;
   onBack?: () => void;
 };
 
@@ -52,6 +56,19 @@ export type SidebarComponentProps = {
 export default function sidebar({ initial, className, onTransitionStart }: Props) {
   const container = div`.sidebar${className}`();
   const wrappersMap = new Map<SidebarState, HTMLElement>();
+  const contextSubjects = new Map<SidebarState, BehaviorSubject<any>>();
+
+  const getContext = <K extends SidebarState>(state: K) => contextSubjects.get(state) as BehaviorSubject<SidebarContext<K>> | undefined;
+
+  const setContext = <K extends SidebarState>(state: K, ctx: SidebarContext<K>) => {
+    let contextSubject = contextSubjects.get(state);
+    if (!contextSubject) {
+      contextSubject = new BehaviorSubject(ctx);
+      contextSubjects.set(state, contextSubject);
+    } else {
+      contextSubject.next(ctx);
+    }
+  };
 
   const popState = () => {
     // just close sidebar
@@ -78,16 +95,18 @@ export default function sidebar({ initial, className, onTransitionStart }: Props
     }
   };
 
-  const pushState = (state: SidebarState) => {
+  const pushState = <K extends SidebarState>(state: K, ctx: SidebarContext<K>) => {
+    setContext(state, ctx);
+
     let element = wrappersMap.get(state);
     if (!element) {
       element = wrapper(
-        elements[state](
+        (elements[state])(
           {
             onNavigate: pushState,
             onBack: popState,
           },
-          main.getSidebarCtx(state)),
+          getContext(state)! as any), // TypeScript will give a combinatorial explosion if you remove `as any`
       );
       wrappersMap.set(state, element);
     }
@@ -115,7 +134,7 @@ export default function sidebar({ initial, className, onTransitionStart }: Props
     }
   });
 
-  if (initial) pushState(initial);
+  if (initial) pushState(initial.state, initial.ctx);
 
   return useInterface(container, { pushState, popState, close });
 }
