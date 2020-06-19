ed340024d0ac0d313b9bc4f0febc9a3f4e65dc66
Sergey Mostsevenko <finesserus@gmail.com>
2020-06-20 00:01:14 +1000

    Add Alert popup

--- src/services/main.ts
+++ src/services/main.ts
@@ -57,6 +57,7 @@ export default class MainService {
   showPopup(type: 'photo', ctx: { rect: DOMRect, options: PhotoOptions, photo: Photo, peer: Peer, message: Message }): void;
   showPopup(type: 'video', ctx: { rect: DOMRect, video: Document.document, peer?: Peer, message?: Message }): void;
   showPopup(type: 'confirmation', ctx: { body: string | Node, title?: string, confirmCallback: () => void }): void;
+  showPopup(type: 'alert', ctx: { body: string | Node, title?: string, callback?: () => void }): void;
   showPopup(type: string, ctx?: any): void {
     this.popupCtx = ctx;
     this.popup.next(type);
