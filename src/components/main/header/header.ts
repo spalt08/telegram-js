8f65babb432bdea6de1e4dc893502e74a6fd38e2
Mikhail Isupov <lam0x86@gmail.com>
2020-06-20 19:45:09 +0300

    Close pinned message button

--- src/components/main/header/header.ts
+++ src/components/main/header/header.ts
@@ -8,8 +8,8 @@ import { contextMenu, formattedMessage, heading, peerFullStatus, ripple, roundBu
 import { listen, mount, unmount, unmountChildren } from 'core/dom';
 import { getInterface, useMaybeObservable, useObservable } from 'core/hooks';
 import { div, nothing, text } from 'core/html';
-import { arePeersSame, peerToId } from 'helpers/api';
-import { Peer } from 'mtproto-js';
+import { arePeersSame, peerToId, messageToId } from 'helpers/api';
+import { Message, Peer } from 'mtproto-js';
 import { main, message } from 'services';
 import './header.scss';
 
@@ -18,6 +18,7 @@ type Props = {
 };
 
 const isMobile = main.window.width < 700;
+const hiddenPinnedMessages = new Set<string>();
 
 let rule: CSSStyleDeclaration | undefined;
 function setStickyOffset(px: number, animate = true) {
@@ -65,22 +66,15 @@ function pinnedMessage(peer: Peer, isAudioActive: boolean) {
   let quote = msg ? messageQuote(msg, 'Pinned message') : undefined;
   let rippleEl: ReturnType<typeof ripple>;
 
-  const container = div`.header__pinned`(
-    rippleEl = ripple({
-      contentClass: 'header__pinned_ripple',
-      onClick: () => msg && message.selectPeer(peer, msg.id),
-    }, [
-      quote || nothing,
-    ]),
-  );
+  const container = div`.header__pinned`();
 
-  pinnedMessageCache.useWatchItem(container, peerToId(peer), (nextMsg) => {
-    if ((nextMsg || {}).id === (msg || {}).id) return;
+  const updatePinnedMessage = (nextMsg: Readonly<Message.message> | undefined) => {
+    if (nextMsg?.id === msg?.id) return;
     msg = nextMsg;
 
     if (quote) unmount(quote);
 
-    if (msg) {
+    if (msg && !hiddenPinnedMessages.has(messageToId(msg))) {
       getInterface(rippleEl).mountChild(quote = messageQuote(msg, 'Pinned message'));
       container.classList.add('-visible');
       updateStickyOffset(true, isAudioActive);
@@ -88,9 +82,31 @@ function pinnedMessage(peer: Peer, isAudioActive: boolean) {
       container.classList.remove('-visible');
       updateStickyOffset(false, isAudioActive);
     }
+  };
+
+  mount(container, rippleEl = ripple({
+    contentClass: 'header__pinned_ripple',
+    onClick: () => msg && message.selectPeer(peer, msg.id),
+  }, [
+    quote || nothing,
+  ]));
+
+  mount(container, roundButton({
+    className: 'header__pinned_close',
+    onClick: () => {
+      if (msg) {
+        hiddenPinnedMessages.add(messageToId(msg));
+        updatePinnedMessage(undefined);
+      }
+    },
+  }, icons.close()),
+  );
+
+  pinnedMessageCache.useWatchItem(container, peerToId(peer), (nextMsg) => {
+    updatePinnedMessage(nextMsg);
   });
 
-  if (quote) {
+  if (quote && msg && !hiddenPinnedMessages.has(messageToId(msg))) {
     updateStickyOffset(true, isAudioActive, false);
     container.classList.add('-visible');
   } else updateStickyOffset(false, isAudioActive, false);
