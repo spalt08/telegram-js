264225aad85e05648f7bc5d0d76add38b2531ff9
Sergey Mostsevenko <finesserus@gmail.com>
2020-06-20 15:11:05 +1000

    Turn pxtorem off
    Because it's not required and creates an excess load for browser

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,12 @@
 module.exports = {
   plugins: {
+    /*
     'postcss-pxtorem': {
       rootValue: 15,
       minPixelValue: 3,
       propList: ['*'],
     },
+    */
     autoprefixer: {},
   },
 };
